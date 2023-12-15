e5e0e6bca0cebe88d21edd2d28fb4ce952278db4
Shaiksadaf17 <shaiksadafpatel786@gmail.com>
2023-12-15 16:47:21 +0530

    Update ViewController.swift

--- xylophone1722/ViewController.swift
+++ xylophone1722/ViewController.swift
@@ -6,15 +6,44 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
-        print("sadaf")
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
+    @IBAction func keyPressed(_ sender: UIButton) {
+        print("Start")
 
+                UIView.animate(withDuration: 0.2) {
+                    sender.alpha = 0.5
+                }
 
-}
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                    self.playSound(note: sender.currentTitle!)
+
+                    sender.alpha = 1.0
+
+                    print("End")
+                }
+            }
+
+            func playSound(note: String) {
+                guard let url = Bundle.main.url(forResource: note, withExtension: "wav") else {
+                    print("Sound file not found for \(note).")
+                    return
+                }
+
+                do {
+                    player = try AVAudioPlayer(contentsOf: url)
+                    player.play()
+                } catch let error {
+                    print("Error creating AVAudioPlayer: \(error.localizedDescription)")
+                }
+            }
+        }
 
